c339941154135af4fa32afd9234972474770f217
duongvancuong <vancuong@279dn@gmail.com>
2018-09-05 15:14:17 +0700

    Init webpack-dashboard

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -11,6 +11,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
+const DashboardPlugin = require('webpack-dashboard/plugin');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
@@ -213,6 +214,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new DashboardPlugin(),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
